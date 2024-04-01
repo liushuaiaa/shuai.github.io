81663ff921ea8fbc99a6f5414fdf586dc6667e80
刘帅 <liushuai@xnewtech.com>
2024-04-01 11:25:33 +0800

    change6

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
+# 删除dist, 否则第二次会提示fatal: A branch named 'main' already exists.
+rm -rf dist
 
 # 发生错误时终止
 set -e
@@ -13,13 +15,12 @@ cd dist
 # echo 'www.example.com' > CNAME
 
 git init
+git checkout -b main
 git add -A
 git commit -m 'deploy'
 
-# 如果你要部署在 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # 如果你要部署在 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:liushuaiaa/shuai.github.io.git master:gh-pages
 
 cd -
